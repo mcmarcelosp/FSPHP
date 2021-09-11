8b6cb90987ef4c4f312d54d852667c07c918c00a
Marcelo Camilo <mcmarcelosp@yahoo.com.br>
2021-09-11 18:00:45 -0300

    adicionei algumas pastas

---
+++ 01-ola-mundo-vamos-começar/index.php
@@ -0,0 +1,2 @@
+<!DOCTYPE html><html lang="pt-br"><head>    <meta charset="UTF-8">
+        <meta name="mit" content="2021-09-11T12:29:45-03:00+191403">    <meta name="viewport" content="width=device-width,initial-scale=1">    <title>FSPHP: Iniciando Um Projeto!</title>    <link rel="stylesheet" href="assets/style.css"/></head><body><?php/** * Bloco PHP com variável start * e comando de saída! */$start = "Vamos Começar!";echo "<h1>Olá Mundo! {$start}</h1>";echo "<p id='js'>Loading...</p>";?><script src="assets/scripts.js"></script></body></html>
