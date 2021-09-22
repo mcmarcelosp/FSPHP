972dcd6abf3b83c58885efb55748ff3f92c5cc6d
Marcelo Camilo <mcmarcelosp@yahoo.com.br>
2021-09-22 15:02:32 -0300

    22/09/2021

--- 03-manipulacao-e-tratamento/03-02-funcoes-para-strings/index.php
+++ 03-manipulacao-e-tratamento/03-02-funcoes-para-strings/index.php
@@ -7,6 +7,17 @@ fullStackPHPClassName("03.02 - Funções para strings");
  */
 fullStackPHPClassSession("strings e multibyte", __LINE__);
 
+$string =  "O último show do AC~m/DC foi incrível!";
+
+var_dump([[
+    "string" => $string,
+    "strlen" => strlen($string),
+    "mb_strlen" => mb_strlen($string),
+    "mb_substr" => mb_substr($string, "9"),
+    "strtoupper" => strtoupper($string),
+    "mb_strtupper" => mb_strtoupper($string),
+
+]]);
 
 /**
  * [ conversão de caixa ] https://php.net/manual/en/function.mb-convert-case.php
